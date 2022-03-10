1450c0f841f71472bd200d0c51378141b506f51f
jairudra <singhhrishi93@gmail.com>
2022-03-10 10:40:49 +0530

    First commit

---
+++ ha_gitpush.sh
@@ -0,0 +1,8 @@
+# Change this to your Home Assistant config folder if it is different
+cd /config
+# Add all files to the repository with respect to .gitignore rules
+git add .
+# Commit changes with message with current date stamp
+git commit -am "config files on `date +'%d-%m-%Y %H:%M:%S'`"
+# Push changes towards GitHub
+git push -u ha1 master
